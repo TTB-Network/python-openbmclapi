fcbd8faffd174f9a2da22eae38752f22fbc7b855
SilianZ <silian_zheng@outlook.com>
2024-03-09 14:39:25 +0800

    :sparkles: 更新

--- container/main.py
+++ container/main.py
@@ -1,26 +1,3 @@
-import importlib
-import subprocess
-
-def install_module(module_name, module = None):
-    module = module or module_name
-    try:
-        importlib.import_module(module_name)
-    except ImportError:
-        print(f"正在安装模块 '{module_name}'...")
-        subprocess.check_call(["pip", "install", module])
-        print(f"模块 '{module_name}' 安装成功")
-
-def init():
-    install_module('socketio')
-    install_module('aiohttp')
-    install_module("hmac")
-    install_module("pyzstd")
-    install_module("avro", "avro-python3")
-
-init()
-
-
-
 import asyncio
 from pathlib import Path
 import ssl
