aa6ed17c0b8be6b87298ed3b883e08372981c1db
tianxiu2b2t <atianxiua@163.com>
2024-04-03 18:19:34 +0800

    Update cluster version

--- core/const.py
+++ core/const.py
@@ -11,7 +11,7 @@ if version_path.exists():
 else:
     VERSION = "Unknown"
 ROOT = os.getcwd()
-API_VERSION = "1.10.2"
+API_VERSION = "1.10.3"
 USER_AGENT = f"openbmclapi-cluster/{API_VERSION} python-openbmclapi/{VERSION}"
 BASE_URL = "https://openbmclapi.bangbang93.com/"
 CLUSTER_ID: str = Config.get("cluster.id")
