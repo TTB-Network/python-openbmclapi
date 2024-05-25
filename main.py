169a4ef308f93bd91554e0c2ea11b13700d36af6
tianxiu2b2t <atianxiua@163.com>
2024-05-25 20:50:19 +0800

    修复没有检测py版本问题

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
-import core
-from core.i18n import locale
 
 if __name__ == "__main__":
     version = sys.version_info
-    if version < (3, 9):
+    from core.i18n import locale
+    if version < (3, 12):
         print(locale.t("main.unsupported_version", 
                        cur=f"{version.major}.{version.minor}.{version.micro}"))
         exit(0)
+    import core
     core.init()
