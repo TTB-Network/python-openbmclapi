8938bf39c15635f9e5a06df35bd494e60807666b
tianxiu2b2t <atianxiua@163.com>
2024-05-25 20:58:11 +0800

    忘记修改回来了

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     version = sys.version_info
     from core.i18n import locale
-    if version < (3, 12):
+    if version < (3, 9):
         print(locale.t("main.unsupported_version", 
                        cur=f"{version.major}.{version.minor}.{version.micro}"))
         exit(0)
