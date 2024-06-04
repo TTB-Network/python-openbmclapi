a9df781c6d2aca4be9a0116b7d63e4d103a65360
tianxiu2b2t <atianxiua@163.com>
2024-06-05 01:03:55 +0800

    chore: 更新 openbmclapi 版本至 1.10.9

--- core/const.py
+++ core/const.py
@@ -20,7 +20,7 @@ else:
 CACHE_BUFFER_COMPRESSION_MIN_LENGTH: int = 64
 DEBUG: bool = Config.get("advanced.debug")
 ROOT: str = os.getcwd()
-API_VERSION: str = "1.10.8"
+API_VERSION: str = "1.10.9"
 USER_AGENT: str = f"openbmclapi-cluster/{API_VERSION} python-openbmclapi/{VERSION}"
 BASE_URL: str = Config.get("advanced.url", "https://openbmclapi.bangbang93.com/")
 BD_URL: str = BASE_URL.replace("openbmclapi", "bd")
