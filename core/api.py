f9c7818596b33f7332901bf3c3767f9ad7e01e40
SilianZ <silian_zheng@outlook.com>
2024-09-03 18:48:36 +0800

    fix: 修复漏洞

--- core/api.py
+++ core/api.py
@@ -1,11 +1,13 @@
 from core.orm import *
 from aiohttp import web
-from core.cluster import VERSION, API_VERSION
+from core.config import Config
+import toml
 import os
 import platform
 import psutil
 
-
+API_VERSION = Config.get("advanced.api_version")
+VERSION = toml.loads(open("pyproject.toml", "r").read())["tool"]["poetry"]["version"]
 async def getStatus(cluster) -> web.Response:
     hourly_hits = getHourlyHits()
     daily_hits = getDailyHits()
