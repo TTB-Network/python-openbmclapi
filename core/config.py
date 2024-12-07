a4ac9eea43ada8dc10c8a16a3b5757abde8d4761
tianxiu2b2t <atianxiua@163.com>
2024-12-08 02:04:28 +0800

    chore: 更个版本号

--- core/config.py
+++ core/config.py
@@ -161,7 +161,7 @@ class Const:
 
 const = Const()
 
-VERSION = "3.2.0"
+VERSION = "3.2.1"
 API_VERSION = "1.13.1"
 USER_AGENT = f"openbmclapi/{API_VERSION} python-openbmclapi/{VERSION}"
 PYTHON_VERSION = ".".join(map(str, (sys.version_info.major, sys.version_info.minor, sys.version_info.micro)))
